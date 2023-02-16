b9af32d3172daa5db9ef3398de48c111761af99a
Alicia Gandiwa <aliciagandiwa@outlook.com>
2023-02-16 09:50:21 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #!/bin/bash
+#include <stdio.h>
+
 /**
  * main - void
  * 
  * return: return 0 if successful or 1 if failed
  */
-puts("\"Programming is like building a multilingual puzzle);
 
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle);
+	 retun (0);
+
+}
