22bf3d1e8a703acde0a5c500d0001eaa379ebcfd
Alicia Gandiwa <aliciagandiwa@outlook.com>
2023-02-16 10:36:52 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
+#include <stdio.h> 
+
 
 /**
- * main  prints "Programming is like building a multilingual puzzle.
- * 
- * Return: always 0.
+ * main - prints "Programming is like building a multilingual puzzle,
+ * follwed by a new line.
+ * Return: Always 0.
  */
 
+
 int main(void)
+
+
 {
-         puts("\"Programming is like building a multilingual puzzle);
-	 retun (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        retun(0);
 
 }
