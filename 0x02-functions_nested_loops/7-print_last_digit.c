fce1b3eec8944fc21b31f8c28d73a1e1cf1c4f4a
Alicia Gandiwa <aliciagandiwa@outlook.com>
2023-02-21 12:57:17 -0800

    times table

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @b: The number to be treated
+ * Return: The value of the last digit
+ */
+int print_last_digit(int b)
+{
+	int last;
+
+	last = b % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
