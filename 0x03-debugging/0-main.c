72af0df8d6ea2a2a900e57931539a8b8f6af3045
Alicia Gandiwa <aliciagandiwa@outlook.com>
2023-02-23 14:43:35 -0800

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Test function that prints if integer is positve or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
