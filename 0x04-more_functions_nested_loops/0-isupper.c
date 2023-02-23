50bf4c58d18f31ff1bc305cbd766053a1746ee8c
Alicia Gandiwa <aliciagandiwa@outlook.com>
2023-02-23 02:10:08 -0800

    issupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _issupper - Checks if letter is an uppercase
+ * @x: The number to be checked
+ * Reurn: 1 for upper letter or 0 for otherwise
+ */
+
+int _issupper(int x)
+
+{
+	if(x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
